89341b8d1420f09d9aec3bad4da1378d38f23c68
mikaelf5 <muribez@unsa.edu.pe>
2026-08-01 21:08:24 -0500

    Update Tabla_hash.h

--- model/Tabla_hash.h
+++ model/Tabla_hash.h
@@ -5,42 +5,75 @@
 #include "Lista_enlazada.h"
 #define SIZE 100000
 
+template <typename V>
 class Hash{
 
     public:
-        Lista_enlazada* id_list;
+        Lista_enlazada<V>* id_list;
         long long size;
+        long long cantidad; 
 
-        Hash();
+        Hash(long long capacidad = SIZE);
         ~Hash();
         long long hash_funcion(long long);
-        void insertar(long long);
+
+        void insertar(long long id, V valor);
+        bool buscar(long long id, V& resultado);
+        bool eliminar(long long id);
+        bool contiene(long long id);
+
+        long long tamano() const;
 
 };
 
-Hash::Hash(){
-    size = SIZE;
-    id_list = new Lista_enlazada[size];
+template <typename V>
+Hash<V>::Hash(long long capacidad){
+    size = capacidad;
+    cantidad = 0;
+    id_list = new Lista_enlazada<V>[size];
 }
 
-Lista_enlazada::~Lista_enlazada(){
-    Nodo* actual = heap;
-    while(actual){
-        Nodo* siguiente = actual->next;
-        delete actual;
-        actual = siguiente;
-    }
+template <typename V>
+Hash<V>::~Hash(){
+    delete[] id_list;
 }
 
-long long Hash::hash_funcion(long long id){
+template <typename V>
+long long Hash<V>::hash_funcion(long long id){
     return id % size;
 }
 
-void Hash::insertar(long long data){
-    long long posicion = hash_funcion(data);
-    id_list[posicion].insert(data);
-
+template <typename V>
+void Hash<V>::insertar(long long id, V valor){
+    long long posicion = hash_funcion(id);
+    bool nuevo = id_list[posicion].insert(id, valor);
+    cantidad++;
 }
+
+template <typename V>
+bool Hash<V>::buscar(long long id, V& resultado){
+    long long posicion = hash_funcion(id);
+    return id_list[posicion].buscar(id, resultado);
+}
+
+template <typename V>
+bool Hash<V>::eliminar(long long id){
+    long long posicion = hash_funcion(id);
+    bool ok = id_list[posicion].eliminar(id);
+    cantidad--;
+    return ok;
+}
+
+template <typename V>
+bool Hash<V>::contiene(long long id){
+    long long posicion = hash_funcion(id);
+    return id_list[posicion].existe(id);
+}
+
+template <typename V>
+long long Hash<V>::tamano() const {
+     return cantidad; }
+
 
 
 #endif
